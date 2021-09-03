5c958ff92544b1272d5c7be354c05aca9581df31
MyAngelAku <myangel@aku.lu>
2021-09-03 03:01:41 +0200

    Fix spelling

--- dictionary.js
+++ dictionary.js
@@ -1,5 +1,5 @@
 const commandIncorrectParams = `Your inputted parameters for this command are incorrect or not allowed, [${process.env.WIKI_URL} check out the wiki to learn more]`;
-const commandNotFound = `This command does not exists, [${process.env.WIKI_URL} check out the wiki to learn more]`;
+const commandNotFound = `This command does not exist, [${process.env.WIKI_URL} check out the wiki to learn more]`;
 const commandNoPrefix = `No command detected, [${process.env.WIKI_URL} check out the wiki to learn more]`;
 const submit = `Check our [${process.env.SUBMISSIONS_URL} website] to submit beatmaps to the collection`;
 const help = `Check out the [${process.env.WIKI_URL} wiki] to learn about all the available commands`;
